67256cb89b70f7d46b708ad5f0fe08f6e6beb575
Zach Prado <zacharyprado1@gmail.com>
2026-01-29 15:57:08 -0800

    benchmark error recovery

--- crates/sneklsp_parser/benches/parse.rs
+++ crates/sneklsp_parser/benches/parse.rs
@@ -4,6 +4,7 @@ use sneklsp_ast::AstArena;
 const SIMPLE_EXPR: &str = include_str!("../../../testdata/simple/expressions.py");
 const SIMPLE_FUNCTION: &str = include_str!("../../../testdata/simple/functions.py");
 const HELLO_WORLD: &str = include_str!("../../../testdata/simple/hello.py");
+const ERRORS: &str = include_str!("../../../testdata/simple/errors.py");
 
 fn bench_parse(c: &mut Criterion) {
     let mut group = c.benchmark_group("parse");
@@ -24,6 +25,24 @@ fn bench_parse(c: &mut Criterion) {
     group.bench_function("hello_world", |b| {
         let arena = AstArena::new();
         b.iter(|| sneklsp_parser::parse(black_box(HELLO_WORLD), &arena));
+    });
+
+    group.finish();
+}
+
+fn bench_parse_with_errors(c: &mut Criterion) {
+    let mut group = c.benchmark_group("parse_errors");
+
+    group.throughput(Throughput::Bytes(ERRORS.len() as u64));
+    group.bench_function("error_recovery", |b| {
+        let arena = AstArena::new();
+        b.iter(|| sneklsp_parser::parse(black_box(ERRORS), &arena));
+    });
+
+    group.throughput(Throughput::Bytes(SIMPLE_FUNCTION.len() as u64));
+    group.bench_function("valid_file_error_mode", |b| {
+        let arena = AstArena::new();
+        b.iter(|| sneklsp_parser::parse(black_box(SIMPLE_FUNCTION), &arena));
     });
 
     group.finish();
