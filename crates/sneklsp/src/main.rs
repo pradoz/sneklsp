6395d7801b901434585d6bfc66a3eded9514b382
Zach Prado <zacharyprado1@gmail.com>
2026-02-05 18:14:51 -0800

    set lsp_server log level to warn

--- crates/sneklsp/src/main.rs
+++ crates/sneklsp/src/main.rs
@@ -30,7 +30,9 @@ fn main() -> Result<()> {
             tracing_subscriber::fmt()
                 .with_env_filter(
                     tracing_subscriber::EnvFilter::from_default_env()
-                        .add_directive(tracing::Level::DEBUG.into()),
+                        .add_directive(tracing::Level::INFO.into())
+                        .add_directive("lsp_server=warn".parse().unwrap())
+                        .add_directive("sneklsp=info".parse().unwrap()),
                 )
                 .with_writer(std::io::stderr)
                 .with_ansi(false)
